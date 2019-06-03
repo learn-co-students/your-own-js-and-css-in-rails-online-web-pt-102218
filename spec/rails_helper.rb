0e5786e9a68275b293186a94ecb608805ae64d5d
bojosteph <bojosteph@gmail.com>
2019-06-03 15:48:58 -0700

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+Capybara.server = :webrick
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
